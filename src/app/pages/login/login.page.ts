eec45838ff1d6253180fcf2ce1bad8ee0eb43bfe
Paula Palma <paul.palma@duocuc.cl>
2023-11-08 22:49:43 -0300

    Remove registration button click event and update dependencies.

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,10 +2,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormControl, Validators, FormBuilder, FormControlName } from '@angular/forms';
 import { AlertController, NavController } from '@ionic/angular';
-import { createClient } from '@supabase/supabase-js';
 import { Router } from '@angular/router';
 import { ServiceRestService } from 'src/app/services/service-rest.service';
-import { error } from 'console';
+
 
 
 
